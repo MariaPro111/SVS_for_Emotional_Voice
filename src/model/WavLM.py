87503d1c1eaca9e8ed691e413ccdd5ba61b5ffb5
Мария Прохорова <mariaprohorova@MacBook-Pro-Maria.local>
2025-04-24 12:07:40 +0300

    add WavLM model

---
+++ src/model/WavLM.py
@@ -0,0 +1,13 @@
+from transformers import WavLMForXVector, WavLMConfig
+import torch
+import torch.nn as nn
+
+
+class WavLMWrapper(nn.Module):
+    def __new__(cls, config_name="microsoft/wavlm-base-plus-sv", model_name="wavlm"):
+        config = WavLMConfig.from_pretrained(config_name)
+        model = WavLMForXVector(config)
+        model.model_name = model_name
+        return model
+    
+    

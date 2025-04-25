9837a7a899f34e48532f6ce086227fea5b4a5d2c
Мария Прохорова <mariaprohorova@MacBook-Pro-Maria.local>
2025-04-25 04:55:22 +0300

    fix configs to multi task

--- src/transforms/__init__.py
+++ src/transforms/__init__.py
@@ -1,3 +1,4 @@
 from src.transforms.normalize import Normalize1D
 from src.transforms.scale import RandomScale1D
 from src.transforms.feature_extractor import FeatureExtractor
+from src.transforms.noise_aug import NoiseAug
